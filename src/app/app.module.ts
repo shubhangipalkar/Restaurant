af4606515f3002825cd9a6c29b52bccf7b1b2454
shubhangi palkar <shubhpalkar27new@gmail.com>
2021-01-09 23:36:08 +0530

    project shifting

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateDetailComponent } from './update-detail/update-detail.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+  AppComponent,
+    RegisterComponent,
+    UpdateDetailComponent,
+    LoginUserComponent,
+    AddRestoComponent,
+    ListRestoComponent,
+    HeaderComponent
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
